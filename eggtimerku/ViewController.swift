226fbdb82d3e6c6580351984e9e23143b3224a2b
anangkur <anangk97@gmail.com>
2024-10-25 15:26:33 +0700

    feat: implement design and functionality of egg timer

--- eggtimerku/ViewController.swift
+++ eggtimerku/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimer = [
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720,
+    ]
+    
+    var count = 0
+    var timer: Timer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onHardenessClicked(_ sender: UIButton) {
+        startTimer(selectedCount: eggTimer[sender.currentTitle!]!)
+    }
+    
+    private func startTimer(selectedCount: Int) {
+        if let timer = self.timer {
+            timer.invalidate()
+            self.timer = nil
+        }
+        count = selectedCount
+        self.timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(update),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func update() {
+        if (count > 0) {
+            print("count: \(count)")
+            count -= 1
+        }
+    }
 }
 
